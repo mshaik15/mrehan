ff8c1f1756643e22ba89bd2a6e5ad1fc57ccf365
Mohammed Rehan <185856337+mshaik15@users.noreply.github.com>
2025-07-29 23:33:08 -0400

    UpdatE

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import ProjectsSection from "../components/Projects"
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-950 text-white relative">
-      <div className="relative z-10 max-w-sm sm:max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-8 lg:py-12">
+      <div className="relative z-10 max-w-xl sm:max-w-3xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-8 lg:py-12">
         <HeroSection />
         <ProjectsSection />
       </div>
